d2d6ba61b05b345f36bce29ed6ed2aa4753ece65
alestor123 <alestoraldous@gmail.com>
2020-11-28 17:39:03 +0530

    added user color

--- App.js
+++ App.js
@@ -5,6 +5,19 @@ rl = readline.createInterface(process.stdin, process.stdout),
 username = process.argv[2],
 channel = process.argv[3];
 socket.emit('join', { username: username, channel: channel });
+socket.on('message', ({ user, msg, channel }) => function(user, msg, channel = this.channel) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    switch (user) {
+      case 'Info':
+        console.log(`(${chalk.yellow(user)}): ${chalk.yellow(msg)} (${chalk.yellow('channel')}: ${chalk.red(channel)})`);
+   break;
+      default:
+        console.log(`${chalk.green(user)}: ${chalk.green(msg)}`);
+    }
+rl.prompt(true);
+}(user, msg, channel));
+
 function prompt() {
     rl.question(`${chalk.cyan(username)}: `, (input) => {
         socket.emit('message', { user: username, msg: input });
