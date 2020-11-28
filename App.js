aa189f59530b60a816337fd242ebfd3de2c67ab4
alestor123 <alestoraldous@gmail.com>
2020-11-28 17:26:09 +0530

    prompt

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
 var readline = require('readline'),
 socket = require('socket.io-client')('https://dev-talk.glitch.me'),
-chalk = require('chalk');
+chalk = require('chalk'),
+rl = readline.createInterface(process.stdin, process.stdout);
