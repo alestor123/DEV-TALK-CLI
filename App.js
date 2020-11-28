f9d95a854445f8f648237ab0a6e2bc0be989bef5
alestor123 <alestoraldous@gmail.com>
2020-11-28 17:38:40 +0530

    working on talk

--- App.js
+++ App.js
@@ -5,3 +5,10 @@ rl = readline.createInterface(process.stdin, process.stdout),
 username = process.argv[2],
 channel = process.argv[3];
 socket.emit('join', { username: username, channel: channel });
+function prompt() {
+    rl.question(`${chalk.cyan(username)}: `, (input) => {
+        socket.emit('message', { user: username, msg: input });
+        prompt()
+        rl.prompt(true);
+      })
+}
